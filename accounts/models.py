81005f38cf26cb0cdaae905ee5ad436e9dc0836a
gauri <gauriajay18@gmail.com>
2022-03-09 11:46:01 +0530

    employee latest

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here
 class Employee(models.Model):
-    name=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
-    empcode=models.CharField(max_length=250)
-    city=models.CharField(max_length=10)
-    desig=models.CharField(max_length=10)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    name=models.CharField(max_length=20)
+    empcode=models.CharField(max_length=20)
+    city=models.CharField(max_length=20)
+    desig=models.CharField(max_length=20)
     pic=models.ImageField(upload_to='images/', default='images/user.png',null=True,blank=True)
     def __str__(self):
         return self.name
